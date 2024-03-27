e51fe6dc29e187585a4008b2bb26ad8a8b96adb4
Xinrui Ge <xinrui.ge24@gmail.com>
2024-03-27 15:11:57 -0400

    updated array

--- src/Labs/a5/WorkingWithArrays.tsx
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -10,7 +10,8 @@ interface Todo {
 }
 
 function WorkingWithArrays() {
-    const API = "http://localhost:4000/a5/todos";
+    const API_BASE = process.env.REACT_APP_API_BASE;
+    const API = `${API_BASE}/a5/todos`;
     const [todo, setTodo] = useState({id: 1, title: "NodeJS Assignment",
             description: "Create a NodeJS server with ExpressJS",
             due: "2021-09-09",
--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 function WorkingWithObjects() {
+    const API_BASE = process.env.REACT_APP_API_BASE;
+
     const [assignment, setAssignment] = useState({
         id: 1, title: "NodeJS Assignment",
         description: "Create a NodeJS server with ExpressJS",
         due: "2021-10-10", completed: false, score: 0,
       });
-    const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment"
+    const ASSIGNMENT_URL = `${API_BASE}/a5/assignment`
 
     const [module, setModule] = useState({id: 1, name: "Module Name",
     description: "On your own", course: "CS4550",});
-    const MODULE_URL = "http://localhost:4000/a5/module";
+    const MODULE_URL = `${API_BASE}/a5/module`;
 
     const fetchAssignment = async () => {
         const response = await axios.get(`${ASSIGNMENT_URL}`);
@@ -43,11 +45,11 @@ function WorkingWithObjects() {
       <div>Assignment: {JSON.stringify(assignment)}</div><br/>
 
       <h4>Retrieving Objects</h4>
-      <a href="http://localhost:4000/a5/assignment">
+      <a href={`${API_BASE}/a5/assignment`}>
         Get Assignment
       </a>
       <h4>Retrieving Properties</h4>
-      <a href="http://localhost:4000/a5/assignment/title">
+      <a href={`${API_BASE}/a5/assignment/title`}>
         Get Title
       </a>
 
@@ -61,8 +63,8 @@ function WorkingWithObjects() {
         value={assignment.title}/>
 
         <br/> <h3>On Your Own Module</h3>
-        <a href="http://localhost:4000/a5/module">Get Module</a> <br/>
-        <a href="http://localhost:4000/a5/module/name">Get Module Name</a><br/>
+        <a href={`${API_BASE}/a5/module`}>Get Module</a> <br/>
+        <a href={`${API_BASE}/a5/module/name`}>Get Module Name</a><br/>
 
         <a href={`${MODULE_URL}/name/${module.name}`}>
             Update Name
